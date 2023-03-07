b732cb9d76e3bba5a39459045a4e706e4669432d
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-07 11:18:45 +0100

    Finished exercise

---
+++ 04 - Modulo Javascript/39 - Promise errors/exercise.js
@@ -0,0 +1,32 @@
+const isLogged = true;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    try {
+      if (isLogged === true) return resolve(Math.round(Math.random() * 100));
+    } catch (error) {
+      return reject(error);
+    }
+  }, 3000);
+});
+
+promise
+  .then((result) => {
+    const secondPromise = new Promise((resolve, reject) => {
+      try {
+        if (result > 0.5) return resolve(`nome: John \netà: ${result}`);
+      } catch (error) {
+        return reject(error);
+      }
+    });
+    secondPromise
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
