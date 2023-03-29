26373a213d61911c384123da0b8be21484e443bb
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-29 16:16:42 +0200

    Finish exercise

--- 05 - Modulo React/06 - Props-Second prop/my-app/src/welcome.js
+++ 05 - Modulo React/06 - Props-Second prop/my-app/src/welcome.js
@@ -4,8 +4,8 @@ export class Welcome extends React.Component{
     render(){
         return(
             <div>
-            <h1>Welcome, {this.props.name} </h1>
-            <h2>Your age is {this.props.age}</h2>
+            <h4>Welcome, {this.props.name} </h4>
+            <p>Your age is {this.props.age}</p>
             </div>
         )
     }
