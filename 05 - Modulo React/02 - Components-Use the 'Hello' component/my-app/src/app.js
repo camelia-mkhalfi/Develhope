32a98f14f9a5b3d4fad40af25d28145dbd02bd96
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-29 10:32:25 +0200

    Finish exercise

---
+++ 05 - Modulo React/02 - Components-Use the 'Hello' component/my-app/src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Hello} from "./hello";
+
+export function App(){
+    return(
+        <div>
+            <Hello />
+            <Hello />
+        </div>
+    );
+}
