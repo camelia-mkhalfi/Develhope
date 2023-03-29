84565700e9961dcfe4876a2623c66209402b3ec3
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-29 10:43:00 +0200

    Finish exercise

---
+++ 05 - Modulo React/03 - Components-Render the 'Message' component/my-app/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Hello} from "./hello";
+import { Message } from "./message";
+
+export function App(){
+    return(
+        <div>
+            <Hello />
+            <Message />
+        </div>
+    );
+}
