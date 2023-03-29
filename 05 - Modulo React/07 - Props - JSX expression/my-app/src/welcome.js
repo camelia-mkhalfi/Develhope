7138697a6d9cd9d0e8ec477b89a389b52dc3f3bc
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-29 16:53:16 +0200

    Finish exercise

--- 05 - Modulo React/07 - Props - JSX expression/my-app/src/welcome.js
+++ 05 - Modulo React/07 - Props - JSX expression/my-app/src/welcome.js
@@ -4,8 +4,8 @@ export class Welcome extends React.Component{
     render(){
         return(
             <div>
-            <h4>Welcome, {this.props.name} </h4>
-            <p>Your age is {this.props.age}</p>
+            <p>Welcome, <strong>{this.props.name}</strong> </p>
+            <p>Your age is <strong>{this.props.age}</strong></p>
             </div>
         )
     }
