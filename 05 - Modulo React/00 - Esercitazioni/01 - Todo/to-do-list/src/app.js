2b9f7cb0d5437d32923aad2ac980b0077fbbe44f
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-03-31 13:54:56 +0200

    Any bug

--- 05 - Modulo React/00 - Esercitazioni/01 - Todo/to-do-list/src/app.js
+++ 05 - Modulo React/00 - Esercitazioni/01 - Todo/to-do-list/src/app.js
@@ -4,18 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export function App() {
   const [toDo, setToDo] = useState([
-    { id: 1, title: "Task 1", status: false },
-    { id: 2, title: "Task 2", status: false },
+    { id: 1, title: "Task 1", status: false, showDoneButton: true },
+    { id: 2, title: "Task 2", status: false, showDoneButton: true },
   ]);
 
   const [newTask, setNewTask] = useState("");
 
+  const [showUpdateButton, setShowUpdateButton] = useState(false);
+
   const addTask = () => {
     if (newTask) {
       let num = toDo.length + 1;
-      let newEntry = { id: num, title: newTask, status: false };
+      let newEntry = {
+        id: num,
+        title: newTask,
+        status: false,
+        showDoneButton: true,
+      };
       setToDo([...toDo, newEntry]);
       setNewTask("");
+      setShowUpdateButton(false);
     }
   };
 
@@ -24,52 +32,76 @@ export function App() {
     setToDo(newTasks);
   };
 
-  const handleChange = (event) =>{
+  const handleChange = (event) => {
     const newValue = event.target.value;
     setNewTask(newValue);
   };
 
   const doneTask = (id) => {
     let newTasks = toDo.filter((task) => {
-      if(task.id === id){
-        task.status="done";
-      }})
-      setNewTask(newTasks);
+      if (task.id === id) {
+        task.status = "done";
+        task.showDoneButton = false;
+      }
+    });
+    setNewTask(newTasks);
+  };
 
+  const modifyTask = (task) => {
+    deleteTask(task.id);
+    setNewTask(task.title);
+    setShowUpdateButton(true);
   }
 
   return (
     <div className="container App">
-      <br/><br/>
+      <br />
+      <br />
       <h2>Esercitazione ToDo</h2>
-      <br/><br/>
+      <br />
+      <br />
       <div className="row">
         <div className="col">
           <input className="form-control form-control-lg" value={newTask} onChange={handleChange}/>
         </div>
-        <div className="col-auto">
-          <button className="btn btn-lg btn-primary" onClick={addTask}> Add task </button>
-        </div>
+        {!showUpdateButton && (
+          <div className="col-auto">
+            <button className="btn btn-lg btn-primary" onClick={addTask}> Add task </button>
+          </div>
+        )}
+        {showUpdateButton && (
+          <div className="col-auto">
+            <button className="btn btn-lg btn-warning" onClick={addTask}> Modifica </button>
+          </div>
+        )}
       </div>
-      <br/><br/>
+      <br />
+      <br />
       {toDo && toDo.length ? "" : "Nessun compito inserito"}
       {toDo &&
         toDo.map((task, index) => {
           return (
-              <div className="row">
-                <div className="col-9 taskRow">
-                  <div className={task.status ? "done" : ""}>
-                    <span className="taskNumber">{index + 1}</span>
-                    <span className="taskText">{task.title}</span>
-                  </div>
-                </div>
-                <div className="col-auto">
-                  <button className="btn btn-lg btn-success" onClick={() => doneTask(task.id)}>Eseguito</button>
-                </div>
-                <div className="col-auto">
-                  <button className="btn btn-lg btn-danger" onClick={() => deleteTask(task.id)}>Delete</button>
+            <div className="row">
+              <div className="col-8 taskRow">
+                <div className={task.status ? "done" : ""}>
+                  <span className="taskNumber">{index + 1}</span>
+                  <span className="taskText">{task.title}</span>
                 </div>
               </div>
+              {task.showDoneButton && (
+                <div className="col-auto">
+                  <button className="btn btn-lg btn-success" onClick={() => doneTask(task.id)}> Eseguito </button>
+                </div>
+              )}
+              {task.showDoneButton && (
+                <div className="col-auto">
+                  <button className="btn btn-lg btn-warning" onClick={() => modifyTask(task)}> Modifica </button>
+                </div>
+              )}
+              <div className="col-auto">
+                <button className="btn btn-lg btn-danger" onClick={() => deleteTask(task.id)}> Cancella </button>
+              </div>
+            </div>
           );
         })}
     </div>
