fd419e6308c72fe1e0f0775a1df884bf4be3cc66
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-11 19:08:26 +0200

    Exercise finish

---
+++ 05 - Modulo React/22 - Forms - Add a reset button/my-app/src/login.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+export class Login extends React.Component{
+
+  state={
+    name:'',
+    password:'',
+    button: false,
+  }
+
+  handleInput = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onLogin = () => {
+      this.setState({button: true}); 
+  }
+
+  clear = () =>{
+    this.setState({name: '', password: '', button: false}); 
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>State: {this.state.button.toString()}</h3>
+        <input value={this.state.name} placeholder="name" name="name" onChange={this.handleInput} />
+        <input value={this.state.password} placeholder="password" type="password" name="password" onChange={this.handleInput} />
+        <button disabled={!this.state.name || !this.state.password} onClick={this.onLogin}>Login</button>
+        <button  onClick={this.clear}>Reset</button>
+
+      </div>
+    )
+  }
+}
