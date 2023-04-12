2c28566e7e0e90170b7f3cc0271128079047ddf3
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-12 12:36:45 +0200

    Exercise finish

--- 05 - Modulo React/31 - Context - Consume the languageContext/my-app/src/displayLanguage.js
+++ 05 - Modulo React/31 - Context - Consume the languageContext/my-app/src/displayLanguage.js
@@ -1,32 +1,33 @@
 import React from "react";
 import { LanguageContext } from "./languageContext";
+import { Text } from "./text";
 
-export class DisplayLanguage extends React.Component{
-
+export class DisplayLanguage extends React.Component {
   state = {
-    language: 'en',
-  }
+    language: "en",
+  };
 
-  handleLanguageChange = (event) =>{
+  handleLanguageChange = (event) => {
     this.setState({
-      language: event.target.value
-    })
-  }
+      language: event.target.value,
+    });
+  };
 
-  render(){
+  render() {
     return (
       <div>
-        <select value={this.state.language} onChange={this.handleLanguageChange}>
+        <select
+          value={this.state.language}
+          onChange={this.handleLanguageChange}
+        >
           <option value="en">Inglese</option>
           <option value="it">Italiano</option>
         </select>
 
         <LanguageContext.Provider value={this.state.language}>
-          {console.log(this.state.language)}
+          <Text />
         </LanguageContext.Provider>
       </div>
-    )
+    );
   }
-
-
 }
