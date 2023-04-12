7f7931ed3acb62c39edc50a1ecaa0da77221d9fa
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-12 11:21:39 +0200

    Exercise finish

---
+++ 05 - Modulo React/27 - Lists - Reset todo/my-app/src/todolist.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export function Todo() {
+  const [toDo, setToDo] = useState([
+    { id: 1, title: "Task 1" },
+    { id: 2, title: "Task 2" },
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = {
+        id: num,
+        title: newTask,
+        status: false,
+        showDoneButton: true,
+      };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = () => {
+    setToDo([]);
+  };
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setNewTask(newValue);
+  };
+
+  return (
+    <div>
+      <h2>ToDo</h2>
+      <input value={newTask} onChange={handleChange} />
+      <button onClick={addTask}> Add task </button>
+      <button onClick={deleteTask}> Cancella </button>
+
+      <br />
+      <br />
+      {toDo && toDo.length ? (
+        <ul>
+          {toDo.map((task) => (
+            <li key={task.id}>{task.title}</li>
+          ))}
+        </ul>
+      ) : (
+        "Nessun compito inserito"
+      )}
+    </div>
+  );
+}
+
