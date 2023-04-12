368728ad716cf1b7d363d40707d3fcadd3cc37e2
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-12 10:37:39 +0200

    Exercise finish

---
+++ 05 - Modulo React/24 - Forms - Automatic focus/my-app/src/login.js
@@ -0,0 +1,30 @@
+import React, { createRef } from "react";
+
+export class Login extends React.Component {
+  _formRef = createRef();
+
+  componentDidMount() {
+    this._formRef.current.elements.name.focus();
+  }
+
+  onLogin = (event) => {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const password = event.target.elements.password.value;
+
+    console.log(name, password);
+  };
+
+  render() {
+    return (
+      <div>
+        <form ref={this._formRef} onSubmit={this.onLogin}>
+          <input placeholder="name" name="name" />
+          <input placeholder="password" type="password" name="password" />
+          <button type="submit">Login</button>
+          <button type="reset">Reset</button>
+        </form>
+      </div>
+    );
+  }
+}
