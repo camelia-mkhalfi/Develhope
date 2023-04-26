e9d4b5fa4f168ecec18b8e7bb2cee86b56e582f3
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-26 21:21:36 +0200

    Finish

--- 50 - React Router - Add a route to the user path/myapp/src/App.js
+++ 50 - React Router - Add a route to the user path/myapp/src/App.js
@@ -5,7 +5,6 @@ export function App() {
   return (
     <Routes>
       <Route path="/users" element={<GithubUserList />}>
-        <Route index element={<h3>Add a user and select it</h3>}/>
         <Route path="/users/:username" element={<ShowGithubUser />} />
       </Route>
     </Routes>
